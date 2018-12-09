a2a5dea4a2f600f39191a12fc989a5f2b916e590
Raphael Lindegger <raphiluzern@gmx.ch>
2018-12-09 12:04:10 +0100

    Implemented haversine formula

--- src/CalculateDistance.java
+++ src/CalculateDistance.java
@@ -29,10 +29,22 @@ public class CalculateDistance {
         }
     }
 
-    // TODO replace with readl Equation
     private double CalcHaversineDistance(Coordinate point1, Coordinate point2) {
-        Random rand = new Random();
-        return rand.nextDouble();
+    	double R = 6371.0088; // Source value: 6372.8, where does this value come from? exchanged with mean radius from wikipedia.org // Radius in kilometers
+    	double lat1 = point1.getLatitude();
+    	double lon1 = point1.getLongitude();
+    	double lat2 = point2.getLatitude();
+    	double lon2 = point2.getLongitude();
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLon = Math.toRadians(lon2 - lon1);
+        lat1 = Math.toRadians(lat1);
+        lat2 = Math.toRadians(lat2);
+ 
+        double a = Math.pow(Math.sin(dLat / 2),2) + Math.pow(Math.sin(dLon / 2),2) * Math.cos(lat1) * Math.cos(lat2);
+        double c = 2 * Math.asin(Math.sqrt(a));
+        return R * c;
+        
+        // Source: http://rosettacode.org/wiki/Haversine_formula#Java
     }
 
 }
