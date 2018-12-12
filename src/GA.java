ff25bc7ffa7d5a6913020cedd3dfa70373168623
Colin Cina <colincina2@gmail.com>
2018-12-12 13:58:44 +0100

    Added GA structure

---
+++ src/GA.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+/*
+
+ */
+public class GA
+{
+    public static Route crossover(Route r1, Route r2) {
+        return null;
+    }
+
+    public static Route mutate(Route r1) {
+        return null;
+    }
+
+    public static List<Route> reproductionSel(List<Route> pRoutes) {
+        return null;
+    }
+
+    public static List<Route> replacementSel(List<Route> pRoutes) {
+        return null;
+    }
+}
