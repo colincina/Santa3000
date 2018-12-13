66f02df6a7730a0effa89d8fe4538b92abd8d95c
nopech <raphiluzern@gmx.ch>
2018-12-13 15:14:45 +0100

    modified RadomSolution

---
+++ src/RandomSolution.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import
+public class RandomSolution {
+    public static List<Route> solve() {
+        List<Route> solution = new ArrayList<>();
+        List<Integer> sortedGifIDs = new ArrayList<>();
+
+        sortedGifIDs.addAll(StoresGift.sortByLongitudeAsc());
+
+        while (sortedGiftIDs.size() = !0) {
+            solution.add(Route)
+        }
+    }
+}
